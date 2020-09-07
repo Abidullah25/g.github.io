a68c36d02111a14722cb3d9617edd1714ae49928
ABOOD <68436379+Abood025@users.noreply.github.com>
2020-09-07 09:59:31 +0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,14 @@
 var getLocation = "https://www.google.com/search?q="
 
+document.getElementById('change-theme-btn').addEventListener('click', function () {
+    let darkThemeEnabled = document.body.classList.toggle('dark-theme');
+    localStorage.setItem('dark-theme-enabled', darkThemeEnabled);
+});
+
+if (JSON.parse(localStorage.getItem('dark-theme-enabled'))) {
+    document.body.classList.add('dark-theme');
+}
+
 function search(getLocation){
     location = getLocation + encodeURIComponent(document.getElementById('textbox').value);
 }
